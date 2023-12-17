3e9df56d8ae5fd4d1e789a2ada3ee30d7cfc706a
SamuelMolero26 <samueljosemolero63@gmail.com>
2023-12-17 14:31:27 -0600

    Second Commit

--- api.py
+++ api.py
@@ -1,5 +1,9 @@
 from  dotenv import load_dotenv
 import os
+import base64
+from requests import post
+import json
+
 
 load_dotenv() #only loads if "envoriment" file is .env
 
@@ -8,3 +12,26 @@ client_secret = os.getenv("CLIENT_SECRET")
 
 # print(client_id,client_secret)
 
+def get_token():
+    auth_string = client_id +  ":" + client_secret
+    auth_bytes = auth_string.encode("utf-8") 
+    auth_base64 = str(base64.b64encode(auth_bytes),"utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization":"Basic " + auth_base64,
+        "Content-Type":"application/x-www-form-urlencoded",
+    }
+
+    data ={"grant_type":"client_credentials"}
+    result = post(url,headers=headers, data=data)
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+    return token
+
+def get_auth_header(token):
+    return {"Authorization": "Bearer " + token}
+
+
+tokens = get_token()
+print(tokens)
