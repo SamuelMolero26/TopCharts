16f26ea8ec18b08f1018e88d436c7b741694acb8
SamuelMolero26 <samueljosemolero63@gmail.com>
2023-12-17 17:03:20 -0600

    New Update

---
+++ data&scripts/server.py
@@ -0,0 +1,13 @@
+from flask import Flask , jsonify
+
+from fetch import get_genres
+
+app = Flask(__name__)
+
+@app.route('/data&scripts', methods =['GET'])
+def get_data():
+    data = get_genres()
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(port=5000)
